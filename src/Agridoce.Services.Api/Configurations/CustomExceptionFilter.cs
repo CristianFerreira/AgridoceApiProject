b6a873b161e0c28664d120ece6ac564a2b904318
Cristian Ferreira <cristianferreira_gks@hotmail.com>
2020-09-26 14:08:28 -0300

    [+] - add custonExceptionFilter

---
+++ src/Agridoce.Services.Api/Configurations/CustomExceptionFilter.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Net;
+
+namespace Agridoce.Services.Api.Configurations
+{
+    public class CustomExceptionFilter : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            HttpStatusCode status = HttpStatusCode.InternalServerError;
+            var contextException = context.Exception;
+            object result = null;
+            result = new { success = false, errors = contextException };
+            var messageException = contextException.InnerException.InnerException != null ?
+                                   contextException.InnerException.InnerException.Message : contextException.InnerException.Message;
+
+
+
+
+            if (contextException.GetType() == typeof(UnauthorizedAccessException))
+            {
+                status = HttpStatusCode.Unauthorized;
+                result = new
+                {
+                    success = false,
+                    errors = messageException
+                };
+            }
+
+            if (contextException.GetType() == typeof(NullReferenceException))
+            {
+                status = HttpStatusCode.NotFound;
+                result = new
+                {
+                    success = false,
+                    errors = messageException
+                };
+            }
+
+
+            if (contextException.GetType() != typeof(NullReferenceException) &&
+                contextException.GetType() != typeof(UnauthorizedAccessException))
+            {
+
+                result = new
+                {
+                    success = false,
+                    errors = messageException
+                };
+            }
+
+            HttpResponse response = context.HttpContext.Response;
+
+            response.StatusCode = (int)status;
+            response.ContentType = "application/json";
+            context.Result = new JsonResult(result);
+        }
+    }
+}
